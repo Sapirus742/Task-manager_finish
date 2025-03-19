0c6ea387be7806742bc1fac020076767b7302fb1
Сергей Морозов <Tyuiu.MorozovMS@outlook.com>
2025-03-19 15:50:48 +0500

    Single view backend

--- backend/src/common/types.ts
+++ backend/src/common/types.ts
@@ -46,63 +46,60 @@ export enum Competence {
   typescript = 'TypeScript',
   postgresql = 'PostgeSQL',
 }
-  
-  export interface LoginResponseDto {
-    access_token: string;
-    username: string;
-    firstname: string;
-    lastname: string;
-    roles: Role[];
-    userId: number;
-  }
-  export interface SignUpRequestDto {
-    username: string;
-    password: string;
-    firstname: string;
-    lastname: string;
-  
-  }
-  export interface SignupResponseDto {
-    success: boolean;
-  
-  }
-  export interface CreateUserDto {
-    name: string;
-    firstname: string;
-    lastname: string;
-    password: string;
-    roles: Role[];
-    status: UserAccountStatus;
-  
-  }
-  
-  export type UpdateUserDto = Omit<CreateUserDto, 'password'>;
-  
-  export type SecuredUser = {
-    id: number;
-    email: string;
-    firstname: string;
-    lastname: string;
-    roles: Role[];
-    status: UserAccountStatus;
-  
-  };
 
-  export enum TaskStatus {
-    new = 'new',
-    inProgress = 'inProgress',
-    done = 'done',
+export enum TaskStatus {
+  new = 'new',
+  inProgress = 'inProgress',
+  done = 'done',
+}
   
-  }
-  
-  export type TaskDto = {
-    id: number;
-    title: string;
-    status: TaskStatus;
-    createdAt: Date;
-    author: SecuredUser;
-    assignee?: any;
-  
-  };
-  
-  export type CreateUpdateTaskDto = Omit<TaskDto, 'id' | 'createdAt' | 'author'>;
+export interface LoginResponseDto {
+  access_token: string;
+  username: string;
+  firstname: string;
+  lastname: string;
+  roles: Role[];
+  userId: number;
+}
+
+export interface SignUpRequestDto {
+  username: string;
+  password: string;
+  firstname: string;
+  lastname: string;
+}
+
+export interface SignupResponseDto {
+  success: boolean;
+}
+
+export interface CreateUserDto {
+  name: string;
+  firstname: string;
+  lastname: string;
+  password: string;
+  roles: Role[];
+  status: UserAccountStatus;
+}
+
+export type SecuredUser = {
+  id: number;
+  email: string;
+  firstname: string;
+  lastname: string;
+  roles: Role[];
+  status: UserAccountStatus;
+};
+
+export type TaskDto = {
+  id: number;
+  title: string;
+  status: TaskStatus;
+  createdAt: Date;
+  author: SecuredUser;
+  assignee?: any;
+};
+
+export type UpdateUserDto = Omit<CreateUserDto, 'password'>;
+
+export type CreateUpdateTaskDto = Omit<TaskDto, 'id' | 'createdAt' | 'author'>;
