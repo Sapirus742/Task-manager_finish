59fdc331e9de4edc457e56fbc0247b0bbd5093c2
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2025-04-11 11:26:26 +0500

    доп удаление

--- backend/src/orm/migrations/1740825689443-AddInitialIdea.ts
+++ backend/src/orm/migrations/1740825689443-AddInitialIdea.ts
@@ -4,14 +4,11 @@ export class AddInitialIdea1740825689443 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            INSERT INTO "idea" ( name,problem,solution,result,resource,stack,status,created_at) VALUES 
-
         `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            DELETE FROM "idea" WHERE name IN ('Ruality - игра для помощи иностранным студентам', 'Чат-бот в telegram для возвращения информации из отчетов Power BI (Например в виде скрина)')
         `);
     }
 
